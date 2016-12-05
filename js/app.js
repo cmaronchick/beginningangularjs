3b654092ecd3e85e5d74b2e9d9c158c5ec0c2dca
Chris Aronchick <chrisaronchick@gmail.com>
2016-12-05 15:44:17 -0800

    Chapter 2 updates

---
+++ js/app.js
@@ -0,0 +1,10 @@
+var myAppModule = angular.module('myAppModule',[]);
+myAppModule.filter('stripDashes', function() {
+    return function(txt) {
+
+    };
+});
+myAppModule.controller('MyFilterDemoCtrl', function($scope) {
+
+    }
+);
